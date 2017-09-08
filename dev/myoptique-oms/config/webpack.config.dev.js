036e9f347b73b6fa31112ab44db4d6451bccd1b8
Emanuele Manco <emanuele@plugandplaydesign.co.uk>
2017-09-08 13:14:03 +0100

    Integrated SASS in scaffolding.

--- dev/myoptique-oms/config/webpack.config.dev.js
+++ dev/myoptique-oms/config/webpack.config.dev.js
@@ -124,6 +124,14 @@ module.exports = {
         ],
         include: paths.appSrc,
       },
+      {   test: /\.scss$/,
+          use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+          }, {
+          loader: "css-loader" // translates CSS into CommonJS
+          }, {
+          loader: "sass-loader" // compiles Sass to CSS
+          }],
       {
         // "oneOf" will traverse all following loaders until one will
         // match the requirements. When no loader matches it will fall

b102c2c5134920001cf67481827f43b956f3e156
tomCollinson <tom@tom-collinson.com>
2018-01-02 09:40:30 +0000

    working with live auth and a few other live API endpoints. Still to update to remaining live end points available

---
+++ dev/react/src/constants/endpoints.js
@@ -0,0 +1,4 @@
+/* Endpoints to the live API */
+export const baseURL = 'http://mbfoa.dev2.glassesdirecttesting.co.uk/api/v1';
+export const authTokenEndpoint = '/auth/token';
+export const fraudCheckOrders = '/fraud-check-orders';
